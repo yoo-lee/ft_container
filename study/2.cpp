5df0a2bac42e7645e9a8cb687b4cc78cc9d0513d
Yoo-Lee <yuuuukan@gmail.com>
2023-02-12 13:18:24 +0900

    ++

---
+++ study/2.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+int main()
+ {
+  std::string s1 = "12";
+  std::string s2 = "123";
+  
+  //compare the string length
+  bool result = std::lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end());
+    // std::cout << s1.begin() << '\n' ;
+	// std::cout << s1.end() << '\n';
+  
+//   bool result = std::lexicographical_compare(s1.rbegin(),s1.end(), s2.begin(), s2.end());
+  if (result) {
+    std::cout << s1 << " is lexicographically less than " << s2 << '\n';
+  } else {
+    std::cout << s1 << " is not lexicographically less than " << s2 << '\n';
+  }
+  
+  return 0;
+}
